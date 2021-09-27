483b5f4f0848324059f255049545db3ce47189ac
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2021-09-27 09:44:07 +0200

    change entity

--- backend/src/main/java/at/htl/travelagency/entity/Trip.java
+++ backend/src/main/java/at/htl/travelagency/entity/Trip.java
@@ -10,34 +10,26 @@ import java.util.Objects;
 @Table(name = "TA_TRIP")
 public class Trip extends PanacheEntityBase {
 
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
-    @JoinColumn(name = "TRIP_ID")
-    private Trip trip;
+    @Column
+    private double price;
 
-    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
-    @JoinColumn(name = "CUSTOMER_ID")
-    private Customer customer;
+    @Column
+    private String destination;
 
-    @Column(name = "TRIPSTART")
-    private LocalDate tripStart;
-
-    @Column(name = "TRIPEND")
-    private LocalDate tripEnd;
-
+    public Trip(double price, String destination) {
+        this.price = price;
+        this.destination = destination;
+    }
 
     public Trip() {
     }
 
-    public Trip(Trip trip, Customer customer, LocalDate tripStart, LocalDate tripEnd) {
-        this.trip = trip;
-        this.customer = customer;
-        this.tripStart = tripStart;
-        this.tripEnd = tripEnd;
-    }
 
     public Long getId() {
         return id;
@@ -47,59 +39,28 @@ public class Trip extends PanacheEntityBase {
         this.id = id;
     }
 
-    public Trip getTrip() {
-        return trip;
+    public double getPrice() {
+        return price;
     }
 
-    public void setTrip(Trip trip) {
-        this.trip = trip;
+    public void setPrice(double price) {
+        this.price = price;
     }
 
-    public Customer getCustomer() {
-        return customer;
+    public String getDestination() {
+        return destination;
     }
 
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public LocalDate getTripStart() {
-        return tripStart;
-    }
-
-    public void setTripStart(LocalDate tripStart) {
-        this.tripStart = tripStart;
-    }
-
-    public LocalDate getTripEnd() {
-        return tripEnd;
-    }
-
-    public void setTripEnd(LocalDate tripEnd) {
-        this.tripEnd = tripEnd;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Trip trip1 = (Trip) o;
-        return Objects.equals(id, trip1.id) && Objects.equals(trip, trip1.trip) && Objects.equals(customer, trip1.customer) && Objects.equals(tripStart, trip1.tripStart) && Objects.equals(tripEnd, trip1.tripEnd);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, trip, customer, tripStart, tripEnd);
+    public void setDestination(String destination) {
+        this.destination = destination;
     }
 
     @Override
     public String toString() {
         return "Trip{" +
                 "id=" + id +
-                ", trip=" + trip +
-                ", customer=" + customer +
-                ", tripStart=" + tripStart +
-                ", tripEnd=" + tripEnd +
+                ", price=" + price +
+                ", destination='" + destination + '\'' +
                 '}';
     }
 }
